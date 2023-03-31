a8524f93f50af0c8b476efaef1384ad9c4e7d73d
VictorM <vmenzhik132f@gmail.com>
2023-03-31 12:10:03 +0600

    feat: add main project

--- news-site/src/component/ListNews.js
+++ news-site/src/component/ListNews.js
@@ -1,43 +1,89 @@
 import React, { useState, useEffect } from 'react'
 import api from '../config'
-import '../style.css';
+import img1 from './img/img1.png'
 
 function NewsMyApp() {
-    const [product, setProduct] = useState([]);
+    const [post, setPost] = useState([]);
   
     useEffect(() => {
         api.get('/api/news-posts')
         .then((response) => {
-            console.log("response", response.data.data);
-            setProduct(response.data.data);
+            // console.log("response", response.data.data);
+            setPost(response.data.data);
         })
         .catch(error => console.error(error));
     }, []);
     return(
-        <div>
-            <h1>Список статей</h1>
             <ul>
-                {product.map(product => (
-                    <li key={product.id}>
-                        {product.name} - {product.price}
+                {post.map(post => (
+                    <li key={post.id}>
+                        <section className="articles">
+                            <div className="container grid">
+                                <section className="big_collumn">
+
+                                     <article className="main_article">
+                                        <div className="img_container">
+                                            <img 
+                                                src={img1} 
+                                                alt="image" 
+                                                className="article_img"/>
+                                        </div>
+                                        <div className="content_container">
+                                            <span className="article_category main_article_category">
+                                            {post.attributes.category}
+                                            </span>
+
+                                            <h2 className="article_title">{post.attributes.title}</h2>
+                                                    
+                                            <p className="main_article_text">{post.attributes.desciption}</p>
+                                            <span className="article_sourse">Источник</span>
+                                        </div>
+                                    </article>
+
+                                </section>
+                            </div>
+                        </section>
                     </li>
                 ))}
             </ul>
-        </div>
     );
   }
   
-  export default NewsMyApp;
+export default NewsMyApp;
+
+{/* <main className="main">
+		<section className="articles">
+			<div className="container grid">
+				<section className="big_collumn">
+
+					<article className="main_article">
+						<div className="img_container">
+							<img 
+                                src={img1} 
+                                alt="image" 
+                                className="article_img"/>
+						</div>
+						<div className="content_container">
+							<span className="article_category main_article_category">
+								Технологии
+							</span>
+
+							<h2 className="article_title">
+                                Отец жанра. Как уже забытый трип-хоп определяет самую популяр…</h2>
+                                
+							<p className="main_article_text">
+                                Новая мода на топовые наряды необычных цветов. В сезоне – топики, шорты-боксеры, сланцы и сандалии. А также большие солнечные очки и яркая шляпка.</p>
+							<span className="article_sourse main_article_sourse">Источник</span>
+						</div>
+					</article>
+
+				</section>
+			</div>
+		</section>
+	</main> */}
 
 
-// function NewsApp() {
-//     const [product, setProduct] = useState([]);
+  
 
-//     useEffect(() => {
-//         fetch('http://localhost:1337/api/news-posts').then(data => {
-//             console.log(data);
-//         })
-//     })
-// }
 
-// export default NewsApp;
+// {post.attributes.title} {post.attributes.desciption}
--- news-site/src/index.js
+++ news-site/src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    {/* <Menu></Menu> */}
+    <Menu></Menu>
     {/* <Block_news></Block_news> */}
     <ListNews></ListNews>
   </React.StrictMode>
